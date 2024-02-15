77950538ee3364f89675a4cafb33d416a6a61b1f
Rami Benhamida <111929923+Rben2002@users.noreply.github.com>
2024-02-14 23:19:53 -0500

    Added Test case shell files for all classes
    Now we just need to fill them in

---
+++ Project1/src/SkipListTests.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SkipListTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
