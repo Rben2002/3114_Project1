e61852368e58b1a900e62e589e4a51e8d4ef6da7
Rami Benhamida <111929923+Rben2002@users.noreply.github.com>
2024-02-14 23:05:49 -0500

    Fixed all style errors with command proc

--- Project1/src/CommandProcessor.java
+++ Project1/src/CommandProcessor.java
@@ -1,5 +1,6 @@
 /**
- * The purpose of this class is to parse a single line from the command text file
+ * The purpose of this class is to parse a single 
+ * line from the command text file
  * according to the format specified in the project specs.
  * 
  * @author CS Staff
@@ -18,7 +19,7 @@ public class CommandProcessor {
      * exist, so the only constructor takes a database class object to feed
      * commands to.
      * 
-     * @param dataIn
+     * param dataIn
      *            the database object to manipulate
      */
     public CommandProcessor() {
@@ -30,7 +31,8 @@ public class CommandProcessor {
      * This method parses keywords in the line and calls methods in the
      * database as required. Each line command will be specified by one of the
      * keywords to perform the actions. 
-     * These actions are performed on specified objects and include insert, remove,
+     * These actions are performed on specified objects 
+     * and include insert, remove,
      * regionsearch, search, and dump. If the command in the file line is not
      * one of these, an appropriate message will be written in the console. This
      * processor method is called for each line in the file. Note that the
@@ -58,7 +60,7 @@ public class CommandProcessor {
             int h = Integer.parseInt(arr[5]);
             Rectangle insertRec = new Rectangle(x, y, w, h);
             KVPair<String, Rectangle> insertPair = 
-            new KVPair<String, Rectangle>(name, insertRec);
+                new KVPair<String, Rectangle>(name, insertRec);
             data.insert(insertPair);
         }
         // calls the appropriate remove method based on the
@@ -68,12 +70,14 @@ public class CommandProcessor {
             int numParam = arr.length - 1;
             if (numParam == 1) {
                 // Calls remove by name
-                String name = arr[1];
+            	String name = arr[1];
                 data.remove(name);
+                
             }
             else if (numParam == 4) {
-                // Calls remove by coordinates
-                int x = Integer.parseInt(arr[1]);
+                // Calls remove by coordinate, converting string
+                // integers into their Integer equivalent minus whitespace
+            	int x = Integer.parseInt(arr[1]);
                 int y = Integer.parseInt(arr[2]);
                 int w = Integer.parseInt(arr[3]);
                 int h = Integer.parseInt(arr[4]);
@@ -85,7 +89,7 @@ public class CommandProcessor {
         else if (command.equals("regionsearch")) {
             // calls the regionsearch method for a set of coordinates
             // the string integers in the line will be trimmed of whitespace
-            int x = Integer.parseInt(arr[1]);
+        	int x = Integer.parseInt(arr[1]);
             int y = Integer.parseInt(arr[2]);
             int w = Integer.parseInt(arr[4]);
             int h = Integer.parseInt(arr[4]);
@@ -94,18 +98,24 @@ public class CommandProcessor {
         }
         else if (command.equals("intersections")) {
             // calls the intersections method, no parameters to be passed
-            // (see the intersections JavaDoc in the Database class for more information)
-            data.intersections();
-           
+            // (see the intersections JavaDoc in 
+        	// the Database class for more information)
+        	data.intersections();
+        	
         }
         else if (command.equals("search")) {
              // calls the search method for a name of object
-            String name = arr[1];
+        	String name = arr[1];
             data.search(name);
+
            
         }
         else if (command.equals("dump")) {
-            data.dump();
+            // calls the dump method for the database, takes no parameters
+            // (see the dump() JavaDoc in the Database class 
+        	//for more information)
+        	data.dump();
+        	
 
         }
         else {
